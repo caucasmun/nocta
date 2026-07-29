1368b7b82aded67477b6b7dbd887e294c0982cf2
caucasmun <vitalikslavkin250@gmail.com>
2026-07-29 03:19:01 +0300

    fixed icon

--- frontend/src/components/LeftNav/LeftNav.jsx
+++ frontend/src/components/LeftNav/LeftNav.jsx
@@ -18,9 +18,7 @@ function LeftNav() {
                 <div className={cn.navBar}>
                     {/* Logo */}
                     <Link to="/" className={cn.logo}>
-                        <svg width="28" height="28" viewBox="0 0 24 24" fill="#1db954">
-                            <path d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z"/>
-                        </svg>
+                       <img src="/icon.png" alt="" />
                         <span className={cn.logoText}>Nocta</span>
                     </Link>
 
